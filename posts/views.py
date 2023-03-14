58111fb4792aeab0830350aee78c74735757d70d
Ndanema98 <davidnda26@yahoo.co.uk>
2023-03-14 00:08:17 +0000

    Feat: Add postlist class for views.py

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
+
+
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by("date_posted")
+    template_name = "index.html"
+    paginate_by = 4
+
+
 
 # Create your views here.
