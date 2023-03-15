20ff9f73c2c664c8fb52ec7128ee63d6e56d9869
Ndanema98 <davidnda26@yahoo.co.uk>
2023-03-15 13:01:49 +0000

    Fix: Add contect_object_name to views.py and update index.html

--- posts/views.py
+++ posts/views.py
@@ -7,4 +7,5 @@ class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by("date_posted")
     template_name = "index.html"
+    context_object_name = 'post_list'
     paginate_by = 4
