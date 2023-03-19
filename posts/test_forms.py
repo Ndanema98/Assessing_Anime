cb8fe8c3db20ce05ee4942f5916411e55e606c63
Ndanema98 <davidnda26@yahoo.co.uk>
2023-03-19 22:59:57 +0000

    Feat: Add testing for reviewform

--- posts/test_forms.py
+++ posts/test_forms.py
@@ -10,7 +10,7 @@ class TestPostForm(TestCase):
         self.assertIn('title', form.errors.keys())
         self.assertEqual(form.errors['title'][0], 'This field is required.')
 
-    def test_done_field_is_not_required(self):
+    def field_is_not_required(self):
         form = PostForm({'title': 'Test Posts'})
         self.assertTrue(form.is_valid())
 
@@ -18,3 +18,22 @@ class TestPostForm(TestCase):
         form = PostForm()
         self.assertEqual(
             form.Meta.fields, ['title', 'excerpt', 'image', 'status'])
+    
+
+class TestReviewForm(TestCase):
+
+    def test_review_name_is_required(self):
+        form = ReviewForm({'name': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('nmae', form.errors.keys())
+        self.assertEqual(form.errors['name'][0], 'This field is required.')
+
+    def field_is_not_required(self):
+        form = ReviewForm({'name': 'Test Reviews'})
+        self.assertTrue(form.is_valid())
+
+    def test_fields_are_explicit_in_form_metaclass(self):
+        form = ReviewForm()
+        self.assertEqual(
+            form.Meta.fields, ['name', 'email', 'content'])
+    
