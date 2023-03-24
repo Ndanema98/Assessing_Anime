d9c9bf2a2e189418bf88f49ff2b97bdcd4a9db2a
Ndanema98 <davidnda26@yahoo.co.uk>
2023-03-24 23:46:48 +0000

    Feat: Add functionality that like and dislike buttons cant be pressed at the same time.

--- posts/views.py
+++ posts/views.py
@@ -1,74 +1,103 @@
-from django.test import TestCase, Client
-from django.urls import reverse
-from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.views import generic, View
+from django.http import HttpResponseRedirect
 from .models import Post, Review
 from .forms import PostForm, ReviewForm
 
 
-class TestViews(TestCase):
-    def setUp(self):
-        self.client = Client()
-        self.user = User.objects.create_user(
-            username='testuser',
-            email='testuser@example.com',
-            password='testpass'
-        )
-        self.post = Post.objects.create(
-            title='Test post',
-            content='This is a test post content',
-            author=self.user,
-            status=1
-        )
-        self.review = Review.objects.create(
-            content='This is a test review content',
-            author=self.user,
-            post=self.post
-        )
-        self.post_like_url = reverse('posts:like', args=[self.post.id])
-        self.post_dislike_url = reverse('posts:dislike', args=[self.post.id])
-        self.post_list_url = reverse('posts:PostList')
-        self.post_create_url = reverse('posts:create_post')
-        self.post_update_url = reverse('posts:update_post', args=[self.post.id])
-        self.post_delete_url = reverse('posts:delete_post', args=[self.post.id])
-        self.post_review_url = reverse('posts:review', args=[self.post.id])
-        self.post_review_create_url = reverse('posts:create_review', args=[self.post.id])
+class PostLike(View):
+    def post(self, request, post_id):
+        post = get_object_or_404(Post, id=post_id)
 
-    def test_post_list_view(self):
-        response = self.client.get(self.post_list_url)
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'index.html')
+        if post.upvotes.filter(id=request.user.id).exists():
+            post.upvotes.remove(request.user)
+        else:
+            post.upvotes.add(request.user)
+            post.downvotes.remove(request.user)
 
-    def test_post_create_view(self):
-        self.client.login(username='testuser', password='testpass')
-        response = self.client.post(self.post_create_url, {
-            'title': 'New post',
-            'content': 'This is a new post content',
-        })
-        self.assertEquals(response.status_code, 302)
-        self.assertRedirects(response, self.post_list_url)
-
-    def test_post_update_view(self):
-        self.client.login(username='testuser', password='testpass')
-        response = self.client.post(self.post_update_url, {
-            'title': 'Updated post',
-            'content': 'This is an updated post content',
-        })
-        self.assertEquals(response.status_code, 302)
-        self.assertRedirects(response, self.post_list_url)
-
-    def test_post_delete_view(self):
-        self.client.login(username='testuser', password='testpass')
-        response = self.client.post(self.post_delete_url)
-        self.assertEquals(response.status_code, 302)
-        self.assertRedirects(response, self.post_list_url)
-
-    def test_post_like_view(self):
-        self.client.login(username='testuser', password='testpass')
-        response = self.client.post(self.post_like_url)
-        self.assertEquals(response.status_code, 302)
-        self.assertRedirects(response, self.post_review_url)
+        return HttpResponseRedirect(reverse('posts:review', args=[post.id]))
 
 
+class PostDislike(View):
+    def post(self, request, post_id):
+        post = get_object_or_404(Post, id=post_id)
+
+        if post.downvotes.filter(id=request.user.id).exists():
+            post.downvotes.remove(request.user)
+        else:
+            if post.upvotes.filter(id=request.user.id).exists():
+                post.upvotes.remove(request.user)
+            post.downvotes.add(request.user)
+
+        return HttpResponseRedirect(reverse('posts:review', args=[post.id]))
 
 
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by("date_posted")
+    template_name = "index.html"
+    context_object_name = 'post_list'
+    paginate_by = 3
 
+
+def post(request):
+    return HttpResponse('Post view')
+
+
+def review(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    context = {
+        'post': post,
+    }
+    
+    return render(request, 'post_detail.html', context)
+
+
+def create_post(request):
+    form = PostForm(request.POST or None)
+
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.author = request.user
+        post.save()
+        return redirect('posts:PostList')
+
+    return render(request, 'post_form.html', {'form': form})
+
+
+def update_post(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    form = PostForm(request.POST or None, instance=post)
+
+    if form.is_valid():
+        form.save()
+        return redirect('posts:PostList')
+
+    return render(request, 'post_form.html', {'form': form, 'post': post})
+  
+
+def delete_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+
+    if post.author != request.user:
+        messages.error(request, "You don't have permission to delete the post")
+        return redirect('posts:PostDetail', post_id=post_id)
+
+    if request.method == 'POST':
+        post.delete()
+        return redirect('posts:PostList')
+    return render(request, 'post_delete.html', {'post': post})
+
+
+def create_review(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    reviewform = ReviewForm()
+    if request.method == 'POST':
+        reviewform = ReviewForm(request.POST)
+        if reviewform.is_valid():
+            review = reviewform.save(commit=False)
+            review.author = request.user
+            review.post = post
+            review.save()
+            return redirect('posts:review', post_id=post_id)
+    return render(request, 'review_form.html', {'reviewform': reviewform, 'post': post})
