73e19bc64920dc9adb5a00d092d0f9fb18ef874a
Ndanema98 <davidnda26@yahoo.co.uk>
2023-03-25 17:04:27 +0000

    Fix: Ammend code to allow for users who create an account to get redirected to the homepage

--- assessing_anime/settings.py
+++ assessing_anime/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
@@ -91,7 +91,7 @@ TEMPLATES = [
         },
     },
 ]
-
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 WSGI_APPLICATION = 'assessing_anime.wsgi.application'
 
 
